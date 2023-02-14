480c84aa4a977f0df686862270f71968fae93929
Swapna Menon <swapnamenon8@gmail.com>
2023-02-13 22:15:08 -0500

    added prompt 'welcome message' to the javascript file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,29 @@
-// ensure that the javascript code loads. Test to see if the HTML file reads the javascript file correctly.
-// window.alert("This is an alert! JavaScript is running!");
+//  ensure that the javascript code loads. Test to see if the HTML file reads the javascript file correctly.
+//  window.alert("This is an alert! JavaScript is running!");
 
-// Creating a function "fight"
-function fight() {
-  window.alert("The fight has begun!");
-  // Storing user input using variables.
-  var playerName = window.prompt("What is your robot's name?");
-  //window.prompt()- adds an alert where the user can input values.
-}
+//  Creating a function "fight"
+// function fight() {
+//   window.alert("The fight has begun!");
+// Storing user input using variables.
+//   var playerName = window.prompt("What is your robot's name?");
+// window.prompt()- adds an alert where the user can input values.
+// }
+// window.prompt(playerName);
+// fight();
+
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Robot";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+};
+
 fight();
